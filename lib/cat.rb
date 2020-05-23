4c9b8de7bd5312564d207dc09b2b6573c2a425ab
Romena Murgor <romenamurgor@gmail.com>
2020-05-23 19:42:49 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,19 @@
+
+
+require 'pry'
+
 class Cat
   # code goes here
+end 
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def mood
+    @mood
+  end
 end
