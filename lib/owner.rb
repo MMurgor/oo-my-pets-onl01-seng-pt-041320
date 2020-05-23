e9e1bdc6dc99cabe314874ecdefcd40bbab730ae
Romena Murgor <romenamurgor@gmail.com>
2020-05-23 19:58:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -13,59 +13,63 @@ end
     @@owners << self
   end
 
-  def self.all
-    @@all
-  end
-
-  def self.reset_all
-    @@all.clear
-  end
-
-  def self.count 
-    @@all.size
-  end
-
   def say_species
-    "I am a #{species}."
+    "I am a #{@species}."
   end
 
-  def buy_fish(name)
-    fish = Fish.new(name)
-    pets[:fishes] << fish
+  def buy_fish(fish_name)
+    @pets[:fishes] << Fish.new(fish_name)
   end
 
-  def buy_cat(name)
-    cat = Cat.new(name)
-    pets[:cats] << cat
+  def buy_cat(cat_name)
+    @pets[:cats] << Cat.new(cat_name)
   end
 
-  def buy_dog(name)
-    dog = Dog.new(name)
-    pets[:dogs] << dog
+  def buy_dog(dog_name)
+    @pets[:dogs] << Dog.new(dog_name)
   end
 
   def walk_dogs
-    pets[:dogs].map {|dog| dog.mood = "happy"}
-
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
   end
 
   def play_with_cats
-    pets[:cats].map {|cat| cat.mood = "happy"}
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
   end
 
   def feed_fish
-    pets[:fishes].map {|fish| fish.mood = "happy"}
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
   end
 
   def sell_pets
-    pets.each do |type, pets|
-      pets.map {|pet| pet.mood = "nervous"}
+    @pets.each do |type, name_array|
+      name_array.each do |pet|
+        pet.mood = "nervous"
+        #name_array.delete(pet)
+      end 
     end
-    pets.clear
+    @pets = {}
   end
 
   def list_pets
-    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
 
+  def self.all
+    @@owners
+  end
+
+  def self.reset_all
+    @@owners.clear
+  end
+
+  def self. count
+    @@owners.length
+  end
 end
